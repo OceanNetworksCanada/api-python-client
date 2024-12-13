1fea9a0eaf0ed3cd8eb308ed328972e8076e7f39
Kan Fu <fukan.thu@gmail.com>
2024-12-13 11:49:21 -0800

    test: add test for overwriting 0-size file when overwrite=False

--- tests/archive_file/conftest.py
+++ tests/archive_file/conftest.py
@@ -18,3 +18,9 @@ def params_location():
 def params_location_multiple_pages(params_location):
     # rowLimit should be less than the total number of rows.
     return params_location | {"rowLimit": 2}
+
+
+@pytest.fixture
+def params_location_single_file(params_location):
+    # Returned archivefile name should be BPR-Folger-59_20191126T000000.000Z.txt
+    return params_location | {"dateFrom": "2019-11-26", "dateTo": "2019-11-27"}
